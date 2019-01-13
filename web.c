3167d17a81b0cee2fe4af60cab14e200089c2c05
Mikhail Ivanov <ivanomik@fel.cvut.cz>
2019-01-13 16:33:25 +0100

    Update web.c

--- web.c
+++ web.c
@@ -19,62 +19,68 @@ int pE[1024];
 <html>\
 <head>\
 <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />\
-<title>Motor Status Example</title>\
+<title>Motor Status:</title>\
 </head>\
 <body onload='setTimeout(function(){location.reload()}, 100);'>\
-<h1>Motor Status Example</h1>\
+<h1>Motor Status:</h1>\
+<p>X - time (seconds), Y - error (steps), Legend: <span style='color:blue;'>Host</span>,  <span style='color:green;'>Client</span>, <span style='color:red;'>Error</span> </p>\
+<p></p>\
 <object type='image/svg+xml' data='graph.svg'>\
 </body> </html>"
 
 #define HTMLg "<?xml version='1.0'?>\
 		<!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.0//EN' 'http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd'>\
-<svg width='1000' height='300' xmlns='http://www.w3.org/2000/svg'>\
-  <g transform='translate(50,130) scale(1)'>\
+<svg width='2000' height='1000' xmlns='http://www.w3.org/2000/svg'>\
+  <g transform='translate(50,400) scale(1)'>\
     <!-- Now Draw the main X and Y axis -->\
     <g style='stroke-width:2; stroke:black'>\
       <!-- X Axis -->\
-      <path d='M 0 0 L 1250 0 Z'/>\
+      <path d='M 0 0 L 2000 0 Z'/>\
       <!-- Y Axis -->\
-      <path d='M 0 -300 L 0 300 Z'/>\
+      <path d='M 0 -450 L 0 450 Z'/>\
     </g>\
     <g style='fill:none; stroke:#B0B0B0; stroke-width:1; stroke-dasharray:2 4;text-anchor:end; font-size:30'>\
-      <text style='fill:black; stroke:none' x='-1' y='100' >-100</text>\
+    <text style='fill:black; stroke:none' x='-1' y='500' >-500</text>\
+    <text style='fill:black; stroke:none' x='-1' y='400' >-400</text>\
+    <text style='fill:black; stroke:none' x='-1' y='300' >-300</text>\
+	<text style='fill:black; stroke:none' x='-1' y='100' >-100</text>\
       <text style='fill:black; stroke:none' x='-1' y='200' >-200</text>\
       <text style='fill:black; stroke:none' x='-1' y='0' >0</text>\
       <text style='fill:black; stroke:none' x='-1' y='-200' >200</text>\
       <text style='fill:black; stroke:none' x='-1' y='-100' >100</text>\
+      <text style='fill:black; stroke:none' x='-1' y='-300' >300</text>\
+      <text style='fill:black; stroke:none' x='-1' y='-400' >400</text>\
+      <text style='fill:black; stroke:none' x='-1' y='-500' >500</text>\
       <g style='text-anchor:middle'>\
-	<text style='fill:black; stroke:none' x='100' y='20' >100</text>\
-	<text style='fill:black; stroke:none' x='200' y='20' >200</text>\
-	<text style='fill:black; stroke:none' x='300' y='20' >300</text>\
-	<text style='fill:black; stroke:none' x='400' y='20' >400</text>\
-	<text style='fill:black; stroke:none' x='500' y='20' >500</text>\
-	<text style='fill:black; stroke:none' x='600' y='20' >600</text>\
-	<text style='fill:black; stroke:none' x='700' y='20' >700</text>\
-	<text style='fill:black; stroke:none' x='800' y='20' >800</text>\
-	<text style='fill:black; stroke:none' x='900' y='20' >900</text>\
-	<text style='fill:black; stroke:none' x='1000' y='20' >1000</text>\
-	<text style='fill:black; stroke:none' x='1100' y='20' >1100</text>\
+	<text style='fill:black; stroke:none' x='200' y='20' >0.2s</text>\
+	<text style='fill:black; stroke:none' x='400' y='20' >0.4s</text>\
+	<text style='fill:black; stroke:none' x='600' y='20' >0.6s</text>\
+	<text style='fill:black; stroke:none' x='800' y='20' >0.8s</text>\
+	<text style='fill:black; stroke:none' x='1000' y='20' >1.0s</text>\
+	<text style='fill:black; stroke:none' x='1200' y='20' >1.2s</text>\
+	<text style='fill:black; stroke:none' x='1400' y='20' >1.4s</text>\
+	<text style='fill:black; stroke:none' x='1600' y='20' >1.6s</text>\
+	<text style='fill:black; stroke:none' x='1800' y='20' >1.8s</text>\
+	<text style='fill:black; stroke:none' x='2000' y='20' >2.0s</text>\
+	<text style='fill:black; stroke:none' x='2200' y='20' >2.2s</text>\
       </g>\
     </g>\
-    <polyline\
+    <polyline fill='none' stroke='blue' stroke-width='1'\
 	      points='"
     
-#define HTMLge "'\
-	style='stroke:blue; stroke-width: 1; fill : none;'/>\
-   <polyline\
-  	      points='"
+#define HTMLge "' />\
+	<polyline fill='none' stroke='red' stroke-width='1'\
+		points='"
 
-#define HTMLge2 "'\
-	style='stroke:red; stroke-width: 1; fill : none;'/>\
-   <polyline\
-  	      points='"
+
+#define HTMLge2 "' />\
+	<polyline fill='none' stroke='green' stroke-width='1'\
+		  points='"
 		
-#define HTMLgend "'\
-	style='stroke:green; stroke-width: 1; fill : none;'/>\
-  </g>\
-</svg>"
-	
+#define HTMLgend "' />\
+	</g>\
+	</svg>"
+
 void newRequest(int newFd, int *h, int *dataN);
 
 void www(int* s, int *h, int *dataN, int argc, char *argv[])
@@ -125,7 +131,7 @@ void www(int* s, int *h, int *dataN, int argc, char *argv[])
       return;
     }
     else{
-    	printf("new connection on www\n");
+    	//printf("new connection on www\n");
     	taskSafe();
     	newRequest(newFd, &(*h), &(*dataN));
     }
@@ -171,44 +177,46 @@ void newRequest(int newFd, int *h, int *dataN){
 		fprintf(f, "%s", HTMLg); // HTMLge
 		for (a = *dataN; a < 1024; a++){
 			fprintf(f, "%d,    %d\n", x, pM[a]*(-1));
-			x++;
+			x+= 2;
 		}
 		for (a = 0; a < *dataN - 1; a++){
 			fprintf(f, "%d,    %d\n", x, pM[a]*(-1));
-			x++;
+			x+= 2;
 		}
 		fprintf(f, "%d,    %d", x, pM[*dataN]*(-1));
-		fprintf(f, "%s", HTMLge);
+		fprintf(f, "%s", HTMLge2);	
+
 		a = 0;
 		x = 0;
-				
-//		for (a = *dataN; a < 1024; a++){
-//			fprintf(f, "%d,    %d\n", x, pE[a]);
-//			x++;
-//		}
-//		for (a = 0; a < *dataN - 1; a++){
-//			fprintf(f, "%d,    %d\n", x, pE[a]);
-//			x++;
-//		}
-//		fprintf(f, "%d,    %d", x, pE[*dataN]);
-//		printf(f, "%s", HTMLge2);
-//		a = 0;
-//		x = 0;
 		
 		
 		for (a = *dataN; a < 1024; a++){
 			fprintf(f, "%d,    %d\n", x, pS[a]*(-1));
-			x++;
+			x+= 2;
 		}
 		for (a = 0; a < *dataN - 1; a++){
 			fprintf(f, "%d,    %d\n", x, pS[a]*(-1));
-			x++;
+			x+= 2;
 		}
 		fprintf(f, "%d,    %d", x, pS[*dataN]*(-1));
+		fprintf(f, "%s", HTMLge);
+		
+		a = 0;
+		x = 0;
+						
+		for (a = *dataN; a < 1024; a++){
+			fprintf(f, "%d,    %d\n", x, pE[*dataN]);
+			x+= 2;
+		}
+		for (a = 0; a < *dataN - 1; a++){
+			fprintf(f, "%d,    %d\n", x, pE[*dataN]);
+			x+= 2;
+		}
+		fprintf(f, "%d,    %d", x, pE[*dataN]);
 		fprintf(f, "%s", HTMLgend);
+		
 	}
 	
 	fclose(f);	
 }
-
 
